951e2bb0000fc5abbd704001a2eeea86dfb49c6c
Richard-L4 <rwells945@gmail.com>
2026-01-06 15:52:18 +0000

    create tournament page, background image

--- england_rugby/urls.py
+++ england_rugby/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # home page
+    path('', views.index, name='index'),
+    path('tournament/', views.tournament, name='tournament'),
 ]
